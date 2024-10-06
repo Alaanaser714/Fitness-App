6a895e9bca2b406f3de2406ee8e5baa67f1d3c02
Alaanaser714 <alaanaser577@gmail.com>
2024-10-06 19:43:18 +0300

    marge one

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
-
-
-import 'package:flutter/cupertino.dart';
+import 'package:fitness_app/view/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+
 //sayed try
 void main() {
-  runApp(const FitnessApp());
+  runApp(const MyApp());
 }
 
-class FitnessApp extends StatelessWidget {
-  const FitnessApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
   }
 }
