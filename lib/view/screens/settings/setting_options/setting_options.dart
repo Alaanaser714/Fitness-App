625716ff87123a0a7ab0f1c09141615ee7722df1
Alaanaser714 <alaanaser577@gmail.com>
2024-10-12 15:15:23 +0300

    solve confilct

--- lib/view/screens/settings/setting_options/setting_options.dart
+++ lib/view/screens/settings/setting_options/setting_options.dart
@@ -1,13 +1,13 @@
 // ignore_for_file: must_be_immutable
 
-import 'package:fitness_app/view/screens/settings/views/account_view.dart';
-import 'package:fitness_app/view/screens/settings/views/feedback_view.dart';
-import 'package:fitness_app/view/screens/settings/views/help_view.dart';
-import 'package:fitness_app/view/screens/settings/views/notifications_view.dart';
-import 'package:fitness_app/view/screens/settings/setting_widgets/setting_types.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/services/authentication/auth_service.dart';
+import '../setting_widgets/setting_types.dart';
+import '../views/account_view.dart';
+import '../views/feedback_view.dart';
+import '../views/help_view.dart';
+import '../views/notifications_view.dart';
 
 class SettingOptions extends StatelessWidget {
   AuthService auth = AuthService();
@@ -20,140 +20,136 @@ class SettingOptions extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 29, 31, 33),
       appBar: AppBar(
-        centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 29, 31, 33),
         iconTheme: const IconThemeData(color: Colors.white),
         title: const Text(
-          'Settings',
+          'Setting',
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 30,
-              ),
-              child: Column(
-                children: [
-                  InkWell(
-                    //Acount page
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const AccountView()),
+      body: Column(
+        children: [
+          const Divider(
+            color: Colors.white,
+            thickness: 0.4,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 30,
+            ),
+            child: Column(
+              children: [
+                InkWell(
+                  //Acount page
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>  AccountView()),
+                  ),
+                  child: getSettingType(icon: Icons.person, text: 'Account'),
+                ),
+                const Divider(
+                  color: Colors.white,
+                  thickness: 0.2,
+                  indent: 35,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  //Help page
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HelpView(),
                     ),
-                    child: getSettingType(icon: Icons.person, text: 'Account'),
                   ),
-                  const Divider(
-                    color: Colors.white,
-                    thickness: 0.2,
-                    indent: 35,
+                  child: getSettingType(
+                    icon: Icons.help,
+                    text: 'Help & Support',
                   ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  InkWell(
-                    //Help page
+                ),
+                const Divider(
+                  color: Colors.white,
+                  thickness: 0.2,
+                  indent: 35,
+                ),
+                const SizedBox(height: 10),
+                InkWell(
+                    //Feedback page
                     onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const HelpView(),
-                      ),
-                    ),
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const FeedbackView(),
+                          ),
+                        ),
+                    child:
+                        getSettingType(icon: Icons.feedback, text: 'Feedback')),
+                const Divider(
+                  color: Colors.white,
+                  thickness: 0.2,
+                  indent: 35,
+                ),
+                const SizedBox(height: 10),
+                InkWell(
+                    //Notification page
+                    onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const NotificationsView(),
+                          ),
+                        ),
                     child: getSettingType(
-                      icon: Icons.help,
-                      text: 'Help & Support',
-                    ),
-                  ),
-                  const Divider(
-                    color: Colors.white,
-                    thickness: 0.2,
-                    indent: 35,
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  InkWell(
-                      //Feedback page
-                      onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const FeedbackView(),
-                            ),
+                        icon: Icons.notifications, text: 'Notifications')),
+                const Divider(
+                  color: Colors.white,
+                  thickness: 0.2,
+                  indent: 35,
+                ),
+                const SizedBox(height: 10),
+                InkWell(
+                  //Logout dialog
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: const Text('Confirm Logout'),
+                        content:
+                            const Text('Are you sure you want to log out?'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Cancel'),
                           ),
-                      child: getSettingType(
-                          icon: Icons.feedback, text: 'Feedback')),
-                  const Divider(
-                    color: Colors.white,
-                    thickness: 0.2,
-                    indent: 35,
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  InkWell(
-                      //Notification page
-                      onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const NotificationsView(),
-                            ),
-                          ),
-                      child: getSettingType(
-                          icon: Icons.notifications, text: 'Notifications')),
-                  const Divider(
-                    color: Colors.white,
-                    thickness: 0.2,
-                    indent: 35,
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  InkWell(
-                    //Logout dialog
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: const Text('Confirm Logout'),
-                          content:
-                              const Text('Are you sure you want to log out?'),
-                          actions: [
-                            TextButton(
+                          TextButton(
                               onPressed: () {
+                                auth.signOut();
                                 Navigator.pop(context);
                               },
-                              child: const Text('Cancel'),
-                            ),
-                            TextButton(
-                                onPressed: () {
-                                  auth.signOut();
-                                  Navigator.pop(context);
-                                },
-                                child: const Text(
-                                  'Log Out',
-                                  style: TextStyle(color: Colors.red),
-                                ))
-                          ],
-                        ),
-                      );
-                    },
-                    child: getSettingType(
-                        icon: Icons.logout,
-                        text: 'LogOut',
-                        textColor: Colors.red,
-                        iconColor: Colors.red),
-                  ),
-                ],
-              ),
+                              child: const Text(
+                                'Log Out',
+                                style: TextStyle(color: Colors.red),
+                              ))
+                        ],
+                      ),
+                    );
+                  },
+                  child: getSettingType(
+                      icon: Icons.logout,
+                      text: 'LogOut',
+                      textColor: Colors.red,
+                      iconColor: Colors.red),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/view/screens/settings/views/account_view.dart
+++ lib/view/screens/settings/views/account_view.dart
@@ -1,9 +1,16 @@
-import 'package:fitness_app/view/screens/settings/setting_widgets/setting_types.dart';
+
+
+// ignore_for_file: must_be_immutable
+
 import 'package:fitness_app/view/screens/settings/views/check_password_view.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core/services/authentication/auth_service.dart';
+import '../setting_widgets/setting_types.dart';
+
 class AccountView extends StatelessWidget {
-  const AccountView({super.key});
+   AccountView({super.key});
+  AuthService auth=AuthService();
 
   @override
   Widget build(BuildContext context) {
@@ -22,23 +29,46 @@ class AccountView extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            InkWell(
-              onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const CheckPasswordView(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+          child: Column(
+            children: [
+              auth.checkSignInMethod()['providerId']=='google.com'?
+               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/Google - png 0.png"),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                   Expanded(
+                     child: Text(
+                      auth.checkSignInMethod()['email'],
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                                       ),
+                   )
+                ],
+              ):
+              InkWell(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>   const CheckPasswordView(),
+                  ),
                 ),
-              ),
-              child: getSettingType(icon: Icons.password, text: 'Password'),
-            ),
-            const Divider(
-              color: Colors.white,
-              indent: 35,
-              thickness: 0.2,
-            )
-          ],
+                child: getSettingType(icon: Icons.password, text: 'Password'),
+              )
+              ,
+              const Divider(
+                color: Colors.white,
+                indent: 35,
+                thickness: 0.2,
+              )
+            ],
+          ),
         ),
       ),
     );
